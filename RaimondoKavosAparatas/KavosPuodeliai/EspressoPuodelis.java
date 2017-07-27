da685a9f8ab70e4c7edb69ac2e8b857b3d090050
raimondassi <raimondassiup@gmail.com>
2017-07-27 20:16:40 +0300

    Raimondo Kavos aparatas

---
+++ RaimondoKavosAparatas/KavosPuodeliai/EspressoPuodelis.java
@@ -0,0 +1,17 @@
+package KavosAparatas.KavosPuodeliai;
+
+import KavosAparatas.*;
+
+
+/**
+ * Created by ozc on 2017.07.25.
+ */
+public class EspressoPuodelis extends KavosPuodelis {
+
+    public EspressoPuodelis() {
+        super("espresso",10,100,100);
+
+
+    }
+
+}
---
+++ RaimondoKavosAparatas/KavosPuodeliai/KavosPavadinimas.java
@@ -0,0 +1,21 @@
+package KavosAparatas.KavosPuodeliai;
+
+import KavosAparatas.*;
+
+/**
+ * Created by ozc on 2017.07.25.
+ */
+public enum KavosPavadinimas {
+    CAPUCINO (new CapucinoPuodelis()), ESPRESSO (new EspressoPuodelis()), LATTE (new LattePuodelis()) ;
+
+    private  KavosPuodelis kavosPuodelis;
+
+    KavosPavadinimas(KavosPuodelis kavosPuodelis){
+        this.kavosPuodelis=kavosPuodelis;
+
+    }
+
+    public KavosPuodelis getKavosPuodelis(){
+        return kavosPuodelis;
+}
+    }
